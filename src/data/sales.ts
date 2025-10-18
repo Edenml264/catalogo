e4319b100c14f38e99731dd1303359565922896d
Edenml264 <edenml264@gmail.com>
2025-10-18 01:35:51 -0700

    Actualización: Se actualizaron los números de teléfono de los vendedores

--- src/data/sales.ts
+++ src/data/sales.ts
@@ -8,27 +8,27 @@ export const salesAgents: SalesAgent[] = [
   {
     name: "Mario Castro",
     email: "ventas@sionline.com.mx",
-    phone: "624-137-7094"
+    phone: "624-217-1765"
   },
   {
     name: "Marco Heredia",
     email: "ventas@sionline.com.mx",
-    phone: "624-147-3891"
+    phone: "624-217-1765"
   },
   {
     name: "Faby Saenz",
     email: "fsaenz@sionline.com.mx",
-    phone: "624-123-4567"
+    phone: "624-217-1765"
   },
   {
     name: "Eden Mendez",
     email: "emendez@sionline.com.mx",
-    phone: "624-166-5588"
+    phone: "624-217-1765"
   },
   {
     name: "Joel Contreras",
     email: "jcontreras@sionline.com.mx",
-    phone: "624-123-4567"
+    phone: "624-217-1765"
   }
   // Puedes agregar más agentes aquí
 ];
